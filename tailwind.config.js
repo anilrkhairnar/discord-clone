4187f4cde16356dadfa13c9ebcbc2c30ba00b3cd
Anil Khairnar <anilrkhairnar@gmail.com>
2023-02-19 20:01:06 +0530

    hero section completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      heading: ["Luckiest Guy", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
